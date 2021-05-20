ced68316fd56eeaac51be00ffee6e26f2e622b0b
Kshitija-rajesh <83940826+Kshitija-rajesh@users.noreply.github.com>
2021-05-20 12:39:31 +0530

    Add files via upload

---
+++ Task 6 - Decision Tree/task 6- Decision tree using R.R
@@ -0,0 +1,34 @@
+### Using Iris Data set for classification
+
+
+
+#### Task By - Kshitija Jagushte
+
+
+
+data("iris")
+View(iris)
+
+### there are 150 datas in the dataset
+
+summary(iris)
+
+### For data partition
+
+set.seed(555)
+
+ind <- sample(2, nrow(iris), replace = TRUE, prob = c(0.8, 0.2))
+
+train <- iris[ind ==1, ]
+test <- iris[ind ==2,]
+
+
+### For Decision Tree Model
+
+library(party)
+library(rpart)   
+library(rpart.plot)      ##For decision tree plot drawing
+
+tree <- rpart(Species ~., train)
+rpart.plot(tree)
+
